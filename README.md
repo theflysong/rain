970f24f4d3ca2e11718d58db2648c6b4090c943b
theflysong <song_of_the_fly@163.com>
2021-06-20 10:24:22 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# rain
+﻿# rain
 
 ## English
 
 A new programming language
 
-If you want to contribute to this project,please check the [pull requests criteria](Criteria_enus.md)
-
+If you want to contribute to this project, Welcome to contribute
 
 ## 简体中文
 
 一个新的编程语言
 
-如果你想为这个出一份力，请查看[提交准则](Criteria_zhcn.md)
+如果你想为这个出一份力，欢迎贡献
 
 ## 繁體中文
 
 一個新的程式設計語言
 
-如果你想為這個出一份力，請查看[提交準則](Criteria_zhtw.md)
+如果你想為這個出一份力，歡迎貢獻
--- RVM/main.cpp
+++ RVM/main.cpp
@@ -2,27 +2,20 @@
 #include "./executer/executer.h"
 #include "./utils/tostringhelper.h"
 #include <iostream>
+#include "lib/rni.h"
+
+RNIValue func(std::vector<RNIValue>) {
+    RNIValue result {new int};
+    *(int*)(result.ptr) = 2;
+    return result;
+}
 
 int main(int argc, char** argv) {
-    Executer executer;
-    //FileByteReader reader(argv[1]);
-    /*RainClass rclass = ClassCreator::createClass(reader);
-    for (std::string con : rclass.const_pool) {
-        std::cout << con << " ";
-    }
-    RainClass::Attributes attr = rclass.attributes;
-    std::cout << std::endl << acceseToString(attr.accese);
-    std::cout << std::endl << toString(attr.is_member);
-    std::cout << std::endl << (int)attr.parent_num;
-    for (short pa : attr.parents) {
-        std::cout << rclass.const_pool[pa] << " ";
-    }*/
-    __ins_init();
-    executer.pushValue(Reference(std::make_shared<Value>(2)));
-    executer.pushValue(Reference(std::make_shared<Value>(7)));
-
-    ins_map[0x07](&executer, Instruction());
-
-    std::cout << executer.popValue().getValue()->getAsNumber() << std::endl;
+    int a = 1, b = 2;
+    std::cout << 1 << std::endl;
+    registryFunction("func", func);
+    RNIValue result = getFunction("func")(std::vector<RNIValue>());
+    std::cout << result.ptr << std::endl;
+    delete result.ptr;
     return 0;
 }
